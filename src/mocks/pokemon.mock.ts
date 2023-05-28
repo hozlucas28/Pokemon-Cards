737323adda8ebaeb3531b84257496b67f934b663
hozlucas28 <hozlucas28@gmail.com>
2023-05-28 16:46:15 -0300

    Create mocks

---
+++ src/mocks/pokemon.mock.ts
@@ -0,0 +1,12 @@
+import { Pokemon } from '../interfaces/Pokemon.interface'
+
+export const pokemon: Pokemon = {
+	id: -1,
+	name: '',
+	image: '',
+	moves: -1,
+	weight: -1,
+	height: -1,
+	loading: true,
+	experience: -1,
+}
